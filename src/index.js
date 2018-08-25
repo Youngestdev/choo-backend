958cb8b4227128efdeeeb6d40822474f4b784375
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2018-08-25 21:26:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,20 +50,6 @@ app.get('/:id', (req, res) => {
   res.send(recipe[0])
 })
 
-const checkToken = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_CLIENT_ID,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
-})
-
 // insert a new recipe
 app.post('/', (req, res) => {
   parseInt(req.body.title, 10).then(
